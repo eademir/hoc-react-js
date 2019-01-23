2c67a3ce0009e8a5ea72c1d9d34adaa1deef5a72
eademir <eraydemir1@yandex.com>
2019-01-24 00:22:30 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
+import Post from './components/posts'
 
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = (
-            posts: [],
+        this.state = {
+            post: [],
             loading: true,
-
-        )
+        }
     }
     componentWillMount() {
-        axios.get()
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(
+            post => post.data
+        ).then(
+            post => {
+                setTimeout(
+                    () =>{
+                        this.setState({
+                            post,
+                            loading: false
+                        })
+                    }, 2000
+                )
+            }
+        )
     }
 
     render() {
     return (
       <div className="App">
-
+          <Post {...this.state}/>
       </div>
     );
   }
--- src/components/posts.js
+++ src/components/posts.js
@@ -1,2 +1,19 @@
 import React from 'react'
 
+export default class Post extends React.Component{
+    render(){
+        return(
+            <div>
+                {
+                    this.props.loading ? <div>loading...</div>:
+                        this.props.post.map(
+                            post =>
+                            <div key={post.id}>
+                                {post.title}
+                            </div>
+                        )
+                }
+            </div>
+        )
+    }
+}
